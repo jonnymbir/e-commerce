419a550443920f050c113626161711b83ac6508b
John Mbir <jonnymbir@gmail.com>
2022-04-05 23:58:49 +0000

    Navbar updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -67,7 +67,7 @@ const Navbar = () => {
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>LAMA.</Logo>
+          <Logo>JAYDEN'S.</Logo>
         </Center>
         <Right>
           <MenuItem>REGISTER</MenuItem>
